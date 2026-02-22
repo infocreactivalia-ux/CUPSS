bed07caa65713821647fb557137f4c18a6af7c38
CREACTIVA <info.creactivalia@gmail.com>
2026-02-22 21:30:15 +0100

    Update page.tsx

---
+++ app/dashboard/compras/page.tsx
@@ -0,0 +1,8 @@
+export default function ComprasPage() {
+  return (
+    <div>
+      <h1 style={{fontSize:'22px',fontWeight:'700',marginBottom:'6px'}}>Marketplace de Compras</h1>
+      <p style={{color:'#9090a8',fontSize:'13px'}}>Módulo en construcción</p>
+    </div>
+  )
+}
